bdbb4482534382b700f012ef6d296ac72fc66276
yuliya <yakimovayuliya@i.ua>
2016-05-16 23:25:51 +0300

    Add tests for card model

---
+++ spec/features/card_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe Card do
+
+  let(:card) { create(:card, original_text: "fox", translated_text: "lisa")}
+
+  it 'create card with empty translated text' do
+    card = Card.create(original_text: 'fox', translated_text: '')
+    expect(card.errors[:translated_text]).to include("can't be blank")
+  end
+
+  it 'create card with empty original text' do
+    card = Card.create(original_text: '', translated_text: 'lisa')
+    expect(card.errors[:original_text]).to include("can't be blank")
+  end
+
+  it "opiginal and tranlation are equal" do
+    card = Card.new(original_text: "fox", translated_text: "fox")
+    expect(card.check_original_and_translated_text).to include("can't be similar as translated text")
+  end
+
+  it "review date is set for today plus 3 days" do
+    expect(card.set_time_to_now).to eq(Date.today + 3.days)
+  end
+
+  it "answer is correct" do
+    expect(card.check_answer("fox")).to be true
+  end
+
+  it "random card is shown on the home page" do
+    card.update_attributes(review_date: Date.today)
+    expect(Card.random_card.nil?).to be false
+  end
+end
