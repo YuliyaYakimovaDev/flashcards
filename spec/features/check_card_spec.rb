69fddb35197c794dca3eeae8c783f74a52e69948
yuliya <yakimovayuliya@i.ua>
2016-05-18 23:47:48 +0300

    first run of integration tests with travis

--- spec/features/check_card_spec.rb
+++ spec/features/check_card_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
-describe Card do
+describe "Check card" do
+
   before(:each) do
-    @card = Card.create(original_text: "dog", translated_text: "sobaka", review_date: Date.today + 3.days)
+    @card = create(:card, original_text: "fox", translated_text: "lisa", review_date: Date.today)
+    visit root_path
+  end
+
+  it "user is redirected to the all cards listing" do
+    click_link 'All cards'
+    expect(page).to have_content "Listing Cards"
+  end
+
+  it "correct answer" do
+    fill_in :answer, with: @card.translated_text
+    click_button 'Check your answer'
+    expect(page).to have_content('Correct!')
+  end
+
+  it "incorrect answer" do
+    fill_in :answer, with: "something"
+    click_button 'Check your answer'
+    expect(page).to have_content('Incorrect!')
   end
 end
